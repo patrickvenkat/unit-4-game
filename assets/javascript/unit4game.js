d5e761adf862df7b1c740e64fe7389fff603853f
Patrick Venkat <patrick.venkat@gmail.com>
2019-07-13 08:51:44 -0400

    Working crystal game

---
+++ assets/javascript/unit4game.js
@@ -0,0 +1,65 @@
+var firstCrystalNumber = Math.floor(Math.random()*4 + 1);
+var secondCrystalNumber = Math.floor(Math.random()*4 + 1);
+var thirdCrystalNumber = Math.floor(Math.random()*4 + 1);
+var fourthCrystalNumber = Math.floor(Math.random()*4 + 1);
+
+// console.log(firstCrystalNumber, secondCrystalNumber, thirdCrystalNumber, fourthCrystalNumber);
+randomNumber = Math.floor(Math.random()*99);
+$("#random-number-box").html("<h2>Random Number: " + randomNumber + "</h2>");
+console.log(randomNumber);
+
+var totalScore = 0;
+var wins = 0;
+var losses =0;
+var gameReset = false;
+
+
+$("#first-crystal").on("click", function(){
+    clickFunction(firstCrystalNumber); 
+});
+$("#second-crystal").on("click", function(){
+    clickFunction(secondCrystalNumber);
+});
+$("#third-crystal").on("click", function(){
+    clickFunction(thirdCrystalNumber);
+});
+$("#fourth-crystal").on("click", function(){
+    clickFunction(fourthCrystalNumber);
+});
+
+function clickFunction(crystalNumber){
+    if (gameReset) {
+        randomNumber = Math.floor(Math.random()*99);
+        $("#random-number-box").html("<h2>Random Number: " + randomNumber + "</h2>");
+        $("#your-total-score").html("<h2>" + "" + "</h2>");
+        firstCrystalNumber = Math.floor(Math.random()*4 + 1);
+        secondCrystalNumber = Math.floor(Math.random()*4 + 1);
+        thirdCrystalNumber = Math.floor(Math.random()*4 + 1);
+        fourthCrystalNumber = Math.floor(Math.random()*4 + 1);
+        gameReset = false;
+    }
+    else {
+        // console.log("entered click function 1");
+        totalScore += crystalNumber;
+        $("#your-total-score").html("<h2>" + totalScore+ "</h2>");
+        winlose();
+    }
+}
+
+function winlose(){
+    if (totalScore === randomNumber){
+        wins ++;
+        $("#wins-losses-box").html("<h3>Wins: " + wins + "</h3>" + "<h3>losses: " + losses + "</h3>");
+        alert("You Won. Click on any crystal to restart the game.");
+        gameReset = true;
+        totalScore = 0;
+    }
+    else if (totalScore > randomNumber){
+        losses ++;
+        $("#wins-losses-box").html("<h3>Wins: " + wins + "</h3>" + "<h3>losses: " + losses + "</h3>");
+        alert("Sorry. You lost!. Click on any crystal to restart the game.");
+        gameReset = true;
+        totalScore = 0;
+
+    }
+};
